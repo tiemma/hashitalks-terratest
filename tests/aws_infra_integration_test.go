8238f01f332a325b95573815275951842b8f7b2a
Bakare Emmanuel <bakare@deimos.co.za>
2020-04-30 10:27:22 +0100

    Corrected typo in test definition

--- tests/aws_infra_integration_test.go
+++ tests/aws_infra_integration_test.go
@@ -1,10 +1,14 @@
 package tests
 
 import (
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/packer"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -13,12 +17,18 @@ import (
 func TestAWSModules(t *testing.T) {
 	t.Parallel()
 
+	packerOptions := &packer.Options{
+		Template: "../modules/packer/builder.json",
+	}
+
+	packerOutput := packer.BuildArtifact(t, packerOptions)
+
 	name := random.UniqueId()
 	environment := "test"
 	used_by := []interface{}{"random_resource"}
 	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
 	terraformOptions := &terraform.Options{
-		TerraformDir: "../examples/security_group",
+		TerraformDir: "../examples",
 		Vars: map[string]interface{}{
 			"name":    name,
 			"used_by": used_by,
@@ -35,6 +45,7 @@ func TestAWSModules(t *testing.T) {
 		},
 	}
 
+	defer aws.DeleteAmiAndAllSnapshots(t, os.Getenv("AWS_REGION"), packerOutput)
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
@@ -45,6 +56,11 @@ func TestAWSModules(t *testing.T) {
 	assert.Equal(t, used_by, output["sg_used_by"])
 	assert.Equal(t, environment, output["sg_tags"].(map[string]interface{})["env"].(string))
 	assert.NotEqual(t, []interface{}{}, output["sg_ingress"])
-	assert.Equal(t, []interface{}{}, output["sg_egress"])
+	assert.NotEqual(t, []interface{}{}, output["sg_egress"])
 	assert.True(t, strings.Contains(output["sg_arn"].(string), "arn"))
+
+	// Instance tests
+	expectedResponse := "!dlorW elloH"
+	assert.Equal(t, output["ec2_instance_state"], "running")
+	http_helper.HttpGetWithValidation(t, fmt.Sprintf("http://%s", output["ec2_public_ip"].(string)), nil, 200, expectedResponse)
 }
